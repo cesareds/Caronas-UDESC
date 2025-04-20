5a687114c1bdaab583594833c0ab6127b3ba280a
cesardesouza <cesarsouza.2002@icloud.com>
2025-04-19 21:57:55 -0300

    feat: criada a classe Cliente, que quando executada é capaz de se comunicar com a Main que está em execução

---
+++ src/Cliente.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Cliente {
+    public static void main(String[] args) throws IOException{
+        Socket socket = new Socket("localhost", 4000);
+        Scanner scanner = new Scanner(System.in);
+
+        PrintStream printStream = new PrintStream(socket.getOutputStream());
+        String entradaDoTeclado = scanner.nextLine();
+        printStream.println(entradaDoTeclado);
+    }
+
+
+}
